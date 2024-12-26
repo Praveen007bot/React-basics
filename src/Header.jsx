fa88c1050e9eff494491995e50df5e96c7f163dc
praveen <praveenani963@gmail.com>
2024-12-26 12:50:17 +0530

    Props concept

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <>
+            <h1>Header Component</h1>
+            <header>some header content</header>
+        </>
+    );
+}
+
+export default Header;
